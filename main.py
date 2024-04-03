98433054af533b1a65bcf2469e314fb04a2537e7
johnnyGogo <johnny.liu@taroko.io>
2024-04-03 17:51:24 +0800

    path operation dependencies

--- main.py
+++ main.py
@@ -122,7 +122,16 @@ class CommonPrams:
   skip: int = 0,
   limit: int = 100
 
-@app.get('/query1/', tags=['Dependencies'])
+async def verify_token(x_token: str = Header()):
+    if x_token != "fake-super-secret-token":
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
+
+async def verify_key(x_key: str = Header()):
+    if x_key != "fake-super-secret-key":
+        raise HTTPException(status_code=400, detail="X-Key header invalid")
+    return x_key
+
+@app.get('/query1/', dependencies=[Depends(verify_token), Depends(verify_key)], tags=['Dependencies'])
 async def query1(commons: CommonPrams = Depends()):
   return commons
 
