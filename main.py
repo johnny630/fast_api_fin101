2f46a3a53e16b1ce1de1a9aeac66a558865cafa7
johnnyGogo <johnny.liu@taroko.io>
2024-04-08 18:27:41 +0800

    add OAuth2 实现简单的 Password 和 Bearer 验证 https://fastapi.tiangolo.com/zh/tutorial/security/simple-oauth2/

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from fastapi.responses import PlainTextResponse
 from fastapi.exceptions import HTTPException, RequestValidationError
 from starlette.responses import HTMLResponse
 from fastapi.encoders import jsonable_encoder
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel, Field, HttpUrl
 from typing import Annotated, Any
 
@@ -15,16 +16,17 @@ from datetime import datetime, time, timedelta
 from uuid import UUID
 import uuid
 
-async def verify_token(x_token: str = Header()):
-    if x_token != "fake-super-secret-token":
-        raise HTTPException(status_code=400, detail="X-Token header invalid")
+# async def verify_token(x_token: str = Header()):
+#     if x_token != "fake-super-secret-token":
+#         raise HTTPException(status_code=400, detail="X-Token header invalid")
 
-async def verify_key(x_key: str = Header()):
-    if x_key != "fake-super-secret-key":
-        raise HTTPException(status_code=400, detail="X-Key header invalid")
-    return x_key
+# async def verify_key(x_key: str = Header()):
+#     if x_key != "fake-super-secret-key":
+#         raise HTTPException(status_code=400, detail="X-Key header invalid")
+#     return x_key
 
-app = FastAPI(dependencies=[Depends(verify_token), Depends(verify_key)])
+# app = FastAPI(dependencies=[Depends(verify_token), Depends(verify_key)])
+app = FastAPI()
 
 BOOKS = [
   {'title': 'johnny book', 'price': 35, 'auth': 'johnny'},
@@ -139,12 +141,81 @@ async def query1(commons: CommonPrams = Depends()):
 async def query2(commons: CommonPrams = Depends()):
   return commons
 
-@app.post("/login/")
-async def login(username: str = Form(), password: str = Form()):
-    if username != 'johnny':
-      raise HTTPException(status_code=418, detail=f"Nope! I don't like {username}.")
+o_auth2_schema = OAuth2PasswordBearer(tokenUrl='token')
 
-    return {'username': username, 'password': password}
+fake_users_db = {
+  "johndoe": {
+    "username": "johndoe",
+    "full_name": "John Doe",
+    "email": "johndoe@example.com",
+    "hashed_password": "fakehashedsecret",
+    "disabled": False,
+  },
+  "alice": {
+    "username": "alice",
+    "full_name": "Alice Wonderson",
+    "email": "alice@example.com",
+    "hashed_password": "fakehashedsecret2",
+    "disabled": True,
+  },
+}
+
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+def fake_hash_password(password: str):
+  return "fakehashed" + password
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+def fake_decode_token(token):
+    # This doesn't provide any security at all
+    # Check the next version
+    user = get_user(fake_users_db, token)
+    return user
+
+async def get_current_user(token: str = Depends(o_auth2_schema)):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+@app.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+  user_dict = fake_users_db.get(form_data.username)
+  if not user_dict:
+      raise HTTPException(status_code=400, detail="Incorrect username or password")
+  user = UserInDB(**user_dict)
+  hashed_password = fake_hash_password(form_data.password)
+  if not hashed_password == user.hashed_password:
+      raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+  return {"access_token": user.username, "token_type": "bearer"}
+
+@app.get("/current_user")
+async def current_user(current_user: User = Depends(get_current_active_user)):
+    return current_user
 
 @app.get("/ping")
 async def ping():
