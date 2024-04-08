c59aa96b3433dc38a3c91c19ad35067a7f7239d0
johnnyliu <bakolama@gmail.com>
2024-04-09 00:06:08 +0800

    Organize login user api

--- main.py
+++ main.py
@@ -175,6 +175,18 @@ class UserInDB(User):
 def fake_hash_password(password: str):
   return "fakehashed" + password
 
+@app.post("/token", tags=['user'])
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+  user_dict = fake_users_db.get(form_data.username)
+  if not user_dict:
+      raise HTTPException(status_code=400, detail="Incorrect username or password")
+  user = UserInDB(**user_dict)
+  hashed_password = fake_hash_password(form_data.password)
+  if not hashed_password == user.hashed_password:
+      raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+  return {"access_token": user.username, "token_type": "bearer"}
+
 def get_user(db, username: str):
     if username in db:
         user_dict = db[username]
@@ -201,19 +213,7 @@ async def get_current_active_user(current_user: User = Depends(get_current_user)
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
-@app.post("/token")
-async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-  user_dict = fake_users_db.get(form_data.username)
-  if not user_dict:
-      raise HTTPException(status_code=400, detail="Incorrect username or password")
-  user = UserInDB(**user_dict)
-  hashed_password = fake_hash_password(form_data.password)
-  if not hashed_password == user.hashed_password:
-      raise HTTPException(status_code=400, detail="Incorrect username or password")
-
-  return {"access_token": user.username, "token_type": "bearer"}
-
-@app.get("/current_user")
+@app.get("/current_user", tags=['user'])
 async def current_user(current_user: User = Depends(get_current_active_user)):
     return current_user
 
