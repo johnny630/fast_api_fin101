32564f73bfc034c48f220987fb55574b8dc0aeec
johnnyGogo <johnny.liu@taroko.io>
2024-05-10 14:55:15 +0800

    add python-dotenv and create get_settings by Config

---
+++ config.py
@@ -0,0 +1,35 @@
+import os
+from functools import lru_cache
+from dotenv import load_dotenv
+
+class BaseConfig:
+    load_dotenv()
+
+    CELERY_BROKER_URL: str = os.environ.get('CELERY_BROKER_URL', 'redis://127.0.0.1:6379/1')
+    CELERY_RESULT_BACKEND: str = os.environ.get('CELERY_RESULT_BACKEND', 'redis://127.0.0.1:6379/2')
+
+class DevelopmentConfig(BaseConfig):
+    pass
+
+
+class ProductionConfig(BaseConfig):
+    pass
+
+
+class TestingConfig(BaseConfig):
+    pass
+
+
+@lru_cache()
+def get_settings():
+    config_cls_dict = {
+        "development": DevelopmentConfig,
+        "production": ProductionConfig,
+        "testing": TestingConfig
+    }
+
+    config_name = os.environ.get("FASTAPI_CONFIG", "development")
+    config_cls = config_cls_dict[config_name]
+    return config_cls()
+
+settings = get_settings()
